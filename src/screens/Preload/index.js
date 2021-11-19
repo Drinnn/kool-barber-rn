6406b05a2c0bc56d41ec195c676bc8d113668734
Drinnn <pedro_lontro@hotmail.com>
2021-11-19 10:26:18 -0300

    feat: add preload screen basic functionality

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import React, { useEffect } from "react";
+import { Container, LoadingIcon } from "./styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import BarberLogo from "../../../assets/barber.svg";
+import { useNavigation } from "@react-navigation/native";
 
 export default () => {
-    return (
-        <Container>
-            <Text>Preload</Text>
-        </Container>
-    )
-}
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem("token");
+      if (token) {
+      } else {
+        navigation.navigate("SignIn");
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  return (
+    <Container>
+      <BarberLogo width="100%" height="160" />
+      <LoadingIcon size="large" color="#fff" />
+    </Container>
+  );
+};
--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack'
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Preload from '../screens/Preload';
-import SignIn from '../screens/Preload';
-import SignUp from '../screens/Preload';
+import Preload from "../screens/Preload";
+import SignIn from "../screens/SignIn";
+import SignUp from "../screens/SignUp";
 
 const Stack = createStackNavigator();
 
 export default () => (
-    <Stack.Navigator>
-        <Stack.Screen name="Preload" component={Preload} />
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-    </Stack.Navigator>
-)
+  <Stack.Navigator
+    initialRouteName="Preload"
+    screenOptions={{ headerShown: false }}
+  >
+    <Stack.Screen name="Preload" component={Preload} />
+    <Stack.Screen name="SignIn" component={SignIn} />
+    <Stack.Screen name="SignUp" component={SignUp} />
+  </Stack.Navigator>
+);
