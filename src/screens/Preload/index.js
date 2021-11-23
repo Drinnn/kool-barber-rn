68b1cff2b150221a7ebd3da5da6a19365cf419b5
Drinnn <pedro_lontro@hotmail.com>
2021-11-23 12:32:07 -0300

    feat: finish preload logic

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,17 +1,39 @@
-import React, { useEffect } from "react";
-import { Container, LoadingIcon } from "./styles";
+import React, { useEffect, useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+import { Container, LoadingIcon } from "./styles";
 import BarberLogo from "../../../assets/barber.svg";
-import { useNavigation } from "@react-navigation/native";
+
+import Api from "../../Api";
+import { UserContext } from "../../contexts/UserContext";
 
 export default () => {
   const navigation = useNavigation();
+
+  const { dispatch: userDispatch } = useContext(UserContext);
 
   useEffect(() => {
     const checkToken = async () => {
       const token = await AsyncStorage.getItem("token");
       if (token) {
+        const res = await Api.checkToken(token);
+        if (res.token) {
+          await AsyncStorage.setItem("token", res.token);
+
+          userDispatch({
+            type: "setAvatar",
+            payload: {
+              avatar: res.data.avatar,
+            },
+          });
+
+          navigation.reset({
+            routes: [{ name: "MainTab" }],
+          });
+        } else {
+          navigation.navigate("SignIn");
+        }
       } else {
         navigation.navigate("SignIn");
       }
